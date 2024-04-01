ebf980466a95c7e4935190fc48dad7480bacf154
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-01 17:09:56 +0100

    use scroll for the projects section

---
+++ components/project.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { projectsData } from "@/lib/data";
+import { useScroll, motion } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
+
+type ProjectProps = (typeof projectsData)[number];
+
+export const Project = ({
+  title,
+  description,
+  tags,
+  imageUrl,
+}: ProjectProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{ scale: scrollYProgress, opacity: scrollYProgress }}
+      className="mb-3 sm:mb-8 last:mb-0"
+    >
+      <section className="group bg-gray-100 max-w-[43rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  hover:bg-gray-200/70 hover:shadow-md transition">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[21.3rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <ul className="flex flex-wrap  mt-4 gap-2 sm:mt-auto">
+            {tags.map((tag, i) => (
+              <li
+                className="bg-black/[0.65] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                key={i}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <Image
+          src={imageUrl}
+          alt={`Project ${title}`}
+          quality={95}
+          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl || group-odd:[initial] group-even:-left-40 || group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 || group-even:group-hover:rotate-2 group-even:group-hover:translate-y-3 group-even:group-hover:translate-x-3 transition"
+        />
+      </section>
+    </motion.div>
+  );
+};
--- components/projects.tsx
+++ components/projects.tsx
@@ -2,6 +2,7 @@ import { projectsData } from "@/lib/data";
 import SectionHeading from "./section-heading";
 import React from "react";
 import Image from "next/image";
+import { Project } from "./project";
 
 export default function Projects() {
   return (
@@ -18,32 +19,5 @@ export default function Projects() {
   );
 }
 
-type ProjectProps = (typeof projectsData)[number];
 
-const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
-  return (
-    <section className="group bg-gray-100 max-w-[43rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 hover:bg-gray-200/70 hover:shadow-md transition">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[21.3rem]">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap  mt-4 gap-2 sm:mt-auto">
-          {tags.map((tag, i) => (
-            <li
-              className="bg-black/[0.65] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-              key={i}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
 
-      <Image
-        src={imageUrl}
-        alt={`Project ${title}`}
-        quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl || group-odd:[initial] group-even:-left-40"
-      />
-    </section>
-  );
-};
